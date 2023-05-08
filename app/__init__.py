18f9b1da3c2ccd98116bba6816eaa3b901e3f7e8
Henry Bach <henrypbach@gmail.com>
2023-05-07 23:09:11 -0400

    not sure

--- app/__init__.py
+++ app/__init__.py
@@ -8,7 +8,6 @@ app.secret_key = "sss"
 def test():
     file = open("static/countries/country_data.json", "r")
     file_data = json.dumps(file.read())
-    print(file_data)
     return render_template('testing.html', file_data = file_data)
 
 if __name__ == "__main__":
