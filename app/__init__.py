452fb8e23a6765cdaac73bb0c60e57a5a7fd77fe
Henry Bach <henrypbach@gmail.com>
2023-05-08 09:22:46 -0400

    We made progress?

--- app/__init__.py
+++ app/__init__.py
@@ -6,9 +6,9 @@ app.secret_key = "sss"
 
 @app.route("/", methods=['GET', 'POST'])
 def test():
-    file = open("static/countries/country_data.json", "r")
-    file_data = json.dumps(file.read())
-    return render_template('testing.html', file_data = file_data)
+    # file = open("static/countries/country_data.json", "r")
+    # file_data = json.dumps(file.read())
+    return render_template('testing.html')#, file_data = file_data)
 
 if __name__ == "__main__":
     app.debug = True
--- app/static/js/world.js
+++ app/static/js/world.js
@@ -2,16 +2,60 @@
 
 // let file = open("../countries/country_data.json");
 //
-// let file_data = file.read();
 
-console.log(file_data);
-let world_data = JSON.parse(file_data);
-console.log(world_data);
 
-let projection = d3.geoOrthographic();
+async function getData() {
+  let data = await fetch('../static/countries/country_data.json');
+  let info = await data.text();
+  return info;
+}
 
-let thingy = d3.geoPath().projection(projection); //Have to look into exactly what this step creates
+async function run() {
+  let file_data = ```
+  { "type": "FeatureCollection",
+  "features": [
+    { "type": "Feature",
+      "geometry": {"type": "Point", "coordinates": [102.0, 0.5]},
+      "properties": {"prop0": "value0"}
+      },
+    { "type": "Feature",
+      "geometry": {
+        "type": "LineString",
+        "coordinates": [
+          [102.0, 0.0], [103.0, 1.0], [104.0, 0.0], [105.0, 1.0]
+          ]
+        },
+      "properties": {
+        "prop0": "value0",
+        "prop1": 0.0
+        }
+      },
+    { "type": "Feature",
+       "geometry": {
+         "type": "Polygon",
+         "coordinates": [
+           [ [100.0, 0.0], [101.0, 0.0], [101.0, 1.0],
+             [100.0, 1.0], [100.0, 0.0] ]
+           ]
 
-let world = d3.select('#globe g.map').selectAll('path').data(world_data.features);
+       },
+       "properties": {
+         "prop0": "value0",
+         "prop1": {"this": "that"}
+         }
+       }
+    ]
+  }```;//await getData();
+  let world_data = await JSON.parse(file_data);
+  console.log(world_data);
 
-world.enter().append('path').attr('d', thingy);
+  let projection = d3.geoOrthographic();
+
+  let thingy = d3.geoPath().projection(projection); //Have to look into exactly what this step creates
+
+  let world = d3.select('#globe g.map').selectAll('path').data(world_data.features);
+
+  world.enter().append('path').attr('d', thingy);
+}
+
+run();
