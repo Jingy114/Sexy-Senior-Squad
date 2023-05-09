f23b2075f6798a9e10b74dd09005b49fb4e23e98
Henry Bach <henrypbach@gmail.com>
2023-05-08 21:54:56 -0400

    Guys its Earth! totally, trust me

--- app/static/js/world.js
+++ app/static/js/world.js
@@ -8,6 +8,7 @@ function getData(callback) {
   fetch('../static/countries/country_data.json')
     .then(response => response.json())
     .then(data => {
+      console.log(data);
       callback(data);
     });
 }
@@ -19,11 +20,12 @@ function run(info) {
 
   let thingy =  d3.geoPath().projection(projection); //Have to look into exactly what this step creates
 
-  let world =  d3.select('#globe g.map').selectAll('path');
-  console.log(world);
-  world =  world.data(world_data.features);
-
-  world.enter().append('path').attr('d', thingy);
+  let world =  d3.select('#globe g.map')
+    .selectAll('path')
+    .data(world_data.geometries)
+    .enter()
+    .append('path')
+    .attr('d', thingy);
 }
 
 getData(run);
