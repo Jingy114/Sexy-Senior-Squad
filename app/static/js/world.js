5e3140824d8f20e867ec8fc89124d28d1f3ae513
Henry Bach <henrypbach@gmail.com>
2023-05-10 12:53:01 -0400

    each country has ID!

--- app/static/js/world.js
+++ app/static/js/world.js
@@ -14,13 +14,29 @@ function run(info) {
 
   let thingy =  d3.geoPath().projection(projection); //Have to look into exactly what this step creates
 
+  let country_list = [];
+
   let world =  d3.select('#globe g.map')
     .selectAll('path')
     .data(world_data.features)
     .enter()
     .append('path')
-    .attr('d', thingy);
-  for (let i = 0; i<world_data.features.length; i++) {
+    .attr('d', thingy)
+    .attr("id", function(d) {
+      country = d.properties.ADMIN;
+      country_list.push(country);
+      return country;
+    });
+  }
+}
+
+function update_colors() {
+
+}
+
+
+    console.log(country_list);
+  for (let i = 0; i<country_list.length; i++) {
     let country = d3.select('#globe g.map')
       .selectAll('path').filter(d => d.properties.ADMIN == country?) //Less efficient way but will be used for now until I can set IDs to each country
       .style("fill", country_color("a"));
