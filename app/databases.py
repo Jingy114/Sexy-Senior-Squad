aae8108ad80827ee5dbbb4f9cf65dc069cc445e3
FaiyazRafee <78169626+FaiyazR7@users.noreply.github.com>
2023-05-17 08:06:19 -0400

    Basic csv file manipulation

--- app/databases.py
+++ app/databases.py
@@ -1,23 +1,43 @@
 import csv
 import sqlite3
 
-# Connect to SQLite database
-conn = sqlite3.connect('my_database.db')
-cur = conn.cursor()
+class DatabaseManager:
+    def __init__(self, db_name):
+        self.conn = sqlite3.connect(db_name)
+        self.cur = self.conn.cursor()
 
-# Read CSV file
-with open('data.csv', 'r') as f:
-    dr = csv.DictReader(f)  # comma is default delimiter
-    headers = dr.fieldnames  # get the headers
+    def create_table_from_csv(self, csv_file, table_name):
+        with open(csv_file, 'r') as f:
+            dr = csv.DictReader(f)
+            headers = dr.fieldnames
+            self.cur.execute(f"DROP TABLE IF EXISTS {table_name}")
+            self.cur.execute(f"CREATE TABLE {table_name} ({', '.join(headers)})")
+            for row in dr:
+                self.cur.execute(f"INSERT INTO {table_name} VALUES ({', '.join('?' * len(headers))})", list(row.values()))
+        self.conn.commit()
 
-    # Create table based on CSV file
-    cur.execute(f"DROP TABLE IF EXISTS my_table")
-    cur.execute(f"CREATE TABLE my_table ({', '.join(headers)})")
+    def select_data(self, table_name, column_name, condition):
+        self.cur.execute(f"SELECT {column_name} FROM {table_name} WHERE {condition}")
+        return self.cur.fetchall()
 
-    # Insert CSV data into the table
-    for row in dr:
-        cur.execute(f"INSERT INTO my_table VALUES ({', '.join('?' * len(headers))})", list(row.values()))
+    def update_data(self, table_name, column_name, new_value, condition):
+        self.cur.execute(f"UPDATE {table_name} SET {column_name} = {new_value} WHERE {condition}")
+        self.conn.commit()
 
-# Commit changes and close connection
-conn.commit()
-conn.close()
+    def close(self):
+        self.conn.close()
+
+
+if __name__ == "__main__":
+    db_manager = DatabaseManager('my_database.db')
+    db_manager.create_table_from_csv('data.csv', 'my_table')
+
+    # Example Usage
+    # Get all rows from 'my_table' where 'country' is 'USA'
+    data = db_manager.select_data('my_table', '*', "country = 'USA'")
+    print(data)
+
+    # Update 'column_name' in 'my_table' to 'new_value' where 'country' is 'USA'
+    db_manager.update_data('my_table', 'column_name', 'new_value', "country = 'USA'")
+
+    db_manager.close()
