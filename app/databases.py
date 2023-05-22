e7d7e55a4ab9ec0929cf02f96282e67163787d80
FaiyazRafee <78169626+FaiyazR7@users.noreply.github.com>
2023-05-22 07:54:50 -0400

    Stuff for testing + sample csv

--- app/databases.py
+++ app/databases.py
@@ -4,7 +4,6 @@ import pandas as pd
 
 # remember to pip install pandas openpyxl
 
-
 def xlsx_to_csv(xlsx_file, csv_file):
     # Read the Excel file
     df = pd.read_excel(xlsx_file)
@@ -12,10 +11,6 @@ def xlsx_to_csv(xlsx_file, csv_file):
     # Write the DataFrame to a CSV file
     df.to_csv(csv_file, index=False)
 
-# Usage
-# xlsx_file = 'input.xlsx'  # your xlsx file
-# csv_file = 'output.csv'  # the output csv file
-# xlsx_to_csv(xlsx_file, csv_file)
 
 class DatabaseManager:
     def __init__(self, db_name):
@@ -37,24 +32,31 @@ class DatabaseManager:
         return self.cur.fetchall()
 
     def update_data(self, table_name, column_name, new_value, condition):
-        self.cur.execute(f"UPDATE {table_name} SET {column_name} = {new_value} WHERE {condition}")
+        self.cur.execute(f"UPDATE {table_name} SET {column_name} = ? WHERE {condition}", (new_value,))
         self.conn.commit()
 
     def close(self):
         self.conn.close()
 
 
-
 if __name__ == "__main__":
     db_manager = DatabaseManager('my_database.db')
-    db_manager.create_table_from_csv('population_by_country_2020.csv', 'my_table')
+    db_manager.create_table_from_csv('sample.csv', 'my_table')
 
-    # Example Usage
-    # Get all rows from 'my_table' where 'country' is 'USA'
+    # Test cases
+    # Get population of China
     data = db_manager.select_data('my_table', 'Population', "Country = 'China'")
-    print(data)
+    print('Population of China:', data)
 
-    # Update 'column_name' in 'my_table' to 'new_value' where 'country' is 'USA'
-    #db_manager.update_data('my_table', 'column_name', 'new_value', "country = 'USA'")
+    # Get all data for USA
+    data = db_manager.select_data('my_table', '*', "Country = 'USA'")
+    print('Data for USA:', data)
+
+    # Update population density of Indonesia
+    db_manager.update_data('my_table', 'Population_Density_PerSqKm', '150', "Country = 'Indonesia'")
+
+    # Check if the update has been successful
+    data = db_manager.select_data('my_table', 'Population_Density_PerSqKm', "Country = 'Indonesia'")
+    print('Updated Population Density for Indonesia:', data)
 
     db_manager.close()
